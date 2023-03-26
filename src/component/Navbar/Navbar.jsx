208d527d1896849c3110a4d54446703660ce56fa
andial61 <andiyusuf1993@gmail.com>
2023-03-26 21:48:01 +0700

    revisi 2

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import './style.css'
 const Navbar = () => {
   return (
     <div className='navbar-container'>
-        <h1>Call a Friend</h1>
-        <h5 className='garis'>your friend contact app</h5>
+        <h1>Telepon Teman</h1>
+        <h5 className='garis'>list friend contact app</h5>
     </div>
   )
 }
